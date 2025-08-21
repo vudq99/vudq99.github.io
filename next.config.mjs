bacf6757d0d0c703b3fd9c9be9143283bc3a5f09
vudq99 <vudq.99@gmail.com>
2025-08-21 16:53:45 +0700

    Update README and other chores

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Export a static HTML app for github public page
   output: 'export',
+
+  // Disable ESLint and TypeScript checks during build
   eslint: {
     ignoreDuringBuilds: true,
   },
